b6c10906c1357d412d89bf0dccb2fc86dc95111e
robmars123 <robmars2008@gmail.com>
2023-03-10 13:53:36 -0600

    Added 5 base classes requirements.

---
+++ DAL/Models/Part.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public abstract class Part
+    {
+        public int PartID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int InStock { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+    }
+}
---
+++ DAL/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Product
+    {
+        public List<Part> AssociatedParts { get; set; }
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int InStock { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public void addAssociatedPart(Part part)
+        {
+            AssociatedParts.Add(part);
+        }
+        public bool removeAssociatedPart(int id)
+        {
+            return false; //false default for now
+        }
+        
+        public Part lookupAssociatedPart(int id)
+        {
+            return AssociatedParts[id];
+        }
+    }
+}
