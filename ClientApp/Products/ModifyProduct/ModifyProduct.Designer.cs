d5de08d7a4f4bffc55e91e1c4dc74801efe29b68
robmars123 <robmars2008@gmail.com>
2023-03-16 10:43:54 -0500

    Fixed error on modifying parts

--- ClientApp/Products/ModifyProduct/ModifyProduct.Designer.cs
+++ ClientApp/Products/ModifyProduct/ModifyProduct.Designer.cs
@@ -233,6 +233,7 @@
             this.btnSearch.TabIndex = 53;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // partsTitle
             // 
